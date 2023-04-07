c0abe7a1155ea2a9c94e8728478b8628e2318b83
sarah sajid <123872994+sarah-sajid2002@users.noreply.github.com>
2023-04-07 18:26:30 +0500

    Add files via upload

---
+++ js practice of functions to find avg of marks/app.js
@@ -0,0 +1,11 @@
+//function to find mean of five numbers:
+
+const meanOfNumbers = (a, b, c, d, e) =>
+{
+    mean = (a+b+c+d+e)/5;
+    return mean;
+}
+meanOfNumbers(2,3,4,5,9);
+console.log(mean);
+
+
